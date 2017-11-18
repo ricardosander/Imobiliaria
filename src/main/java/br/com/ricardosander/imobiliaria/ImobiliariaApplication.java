7763e59bcd4f7544a1ad9e9b35eff056b71fbb06
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-11-17 22:52:42 -0200

    Iniciando projeto.

---
+++ src/main/java/br/com/ricardosander/imobiliaria/ImobiliariaApplication.java
@@ -0,0 +1,12 @@
+package br.com.ricardosander.imobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariaApplication.class, args);
+	}
+}
